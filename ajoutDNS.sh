7caa8cc4f3dabd002e7043071f27529d0740d664
Bouzelmat Lina <152987036+Lina-Bouzelmat@users.noreply.github.com>
2026-01-08 16:05:54 +0100

    Update DNSP IP and subdomain IP in ajoutDNS.sh

--- ajoutDNS.sh
+++ ajoutDNS.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# Script lancé depuis le serveur AMS, ajout d’un sous-domaine dans le DNSP
 
 NOM=$1
 DNSP_USER="stud"
-DNSP_IP="192.168.1.10"
+DNSP_IP="192.168.1.1"
 ZONE_FILE="/etc/bind/db.ceri.com"
-SOUSDOMAINE_LIGNE="$NOM IN A 192.168.1.13"
+SOUSDOMAINE_LIGNE="$NOM IN A 192.168.1.14"
 
-# Étape 1 : Écrire l'entrée dans le fichier zone sur DNSP
+# Dans le fichier zone sur DNSP
 ssh $DNSP_USER@$DNSP_IP "echo '$SOUSDOMAINE_LIGNE' | sudo tee -a $ZONE_FILE"
 
-# Étape 2 : Redémarrer bind9
+# Redemarrer bind9
 ssh $DNSP_USER@$DNSP_IP "sudo systemctl restart bind9"
