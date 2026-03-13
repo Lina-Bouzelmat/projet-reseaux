da32e52230019b9c63d569fbe3ecf969463ad845
Bouzelmat Lina <152987036+Lina-Bouzelmat@users.noreply.github.com>
2026-03-13 12:20:46 +0100

    Refactor database insertion logic in save_regle.php

--- save_regle.php
+++ save_regle.php
@@ -1,20 +1,23 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=ams;charset=utf8", "root", "");
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+require_once("db_natbox.php");
 
 if(
+    isset($_POST['appareil_id'], $_POST['jour'], $_POST['heure_debut'], $_POST['heure_fin']) &&
     !empty($_POST['appareil_id']) &&
     !empty($_POST['jour']) &&
     !empty($_POST['heure_debut']) &&
     !empty($_POST['heure_fin'])
 ){
-    $stmt = $pdo->prepare("INSERT INTO regles_parentales(appareil_id, jour, heure_debut, heure_fin, actif) VALUES(?, ?, ?, ?, 1)");
-    $stmt->execute([
-        $_POST['appareil_id'],
-        $_POST['jour'],
-        $_POST['heure_debut'],
-        $_POST['heure_fin']
-    ]);
+    $appareil_id = $_POST['appareil_id'];
+    $jour = $_POST['jour'];
+    $heure_debut = $_POST['heure_debut'];
+    $heure_fin = $_POST['heure_fin'];
+
+    $stmt = $pdo->prepare("
+        INSERT INTO regles_parentales(appareil_id, jour, heure_debut, heure_fin, actif)
+        VALUES(?, ?, ?, ?, 1)
+    ");
+    $stmt->execute([$appareil_id, $jour, $heure_debut, $heure_fin]);
 }
 
 header("Location: controle_parental.php");
