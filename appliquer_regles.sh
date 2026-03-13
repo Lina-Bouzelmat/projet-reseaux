5fbf0fcfde8afa0b17d7b09450387d013529d467
Bouzelmat Lina <152987036+Lina-Bouzelmat@users.noreply.github.com>
2026-03-13 11:27:29 +0100

    Add script to apply parental control rules

---
+++ appliquer_regles.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DB="ams"
+USER="root"
+PASS=""
+
+jour=$(date +%A)
+heure=$(date +%H:%M:%S)
+
+case "$jour" in
+    Monday) jour_fr="lundi" ;;
+    Tuesday) jour_fr="mardi" ;;
+    Wednesday) jour_fr="mercredi" ;;
+    Thursday) jour_fr="jeudi" ;;
+    Friday) jour_fr="vendredi" ;;
+    Saturday) jour_fr="samedi" ;;
+    Sunday) jour_fr="dimanche" ;;
+    *) exit 0 ;;
+esac
+
+mysql -u"$USER" "$DB" -N -e "
+SELECT a.mac, r.heure_debut, r.heure_fin
+FROM regles_parentales r
+JOIN appareils a ON r.appareil_id = a.id
+WHERE r.actif = 1 AND r.jour = '$jour_fr';
+" | while read mac debut fin
+do
+    if [[ "$heure" > "$debut" && "$heure" < "$fin" ]]; then
+        iptables -C FORWARD -m mac --mac-source "$mac" -j DROP 2>/dev/null
+        if [ $? -ne 0 ]; then
+            iptables -I FORWARD -m mac --mac-source "$mac" -j DROP
+        fi
+    else
+        iptables -D FORWARD -m mac --mac-source "$mac" -j DROP 2>/dev/null
+    fi
+done
