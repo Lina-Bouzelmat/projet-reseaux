0e72fb8021219567098b86988048efcee6c0ab05
Bouzelmat Lina <152987036+Lina-Bouzelmat@users.noreply.github.com>
2026-03-13 12:13:02 +0100

    Create db_natbox.php for database connection setup

---
+++ db_natbox.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$dbname = "natbox_db";
+$user = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Erreur connexion BDD : " . $e->getMessage());
+}
+
+?>
