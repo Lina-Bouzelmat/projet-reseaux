2280310e933d7cfb5bb22d2aa788cb31b9305f43
Bouzelmat Lina <152987036+Lina-Bouzelmat@users.noreply.github.com>
2026-03-13 12:26:10 +0100

    Refactor time variables and MySQL query execution

--- appliquer_regles.sh
+++ appliquer_regles.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-DB="ams"
-USER="root"
-PASS=""
+jour_en=$(date +%A)
+heure_actuelle=$(date +%H:%M:%S)
 
-jour=$(date +%A)
-heure=$(date +%H:%M:%S)
-
-case "$jour" in
+case "$jour_en" in
     Monday) jour_fr="lundi" ;;
     Tuesday) jour_fr="mardi" ;;
     Wednesday) jour_fr="mercredi" ;;
@@ -18,14 +14,14 @@ case "$jour" in
     *) exit 0 ;;
 esac
 
-mysql -u"$USER" "$DB" -N -e "
+mysql -u root -p'ton_mot_de_passe_mysql' natbox_db -N -e "
 SELECT a.mac, r.heure_debut, r.heure_fin
 FROM regles_parentales r
 JOIN appareils a ON r.appareil_id = a.id
 WHERE r.actif = 1 AND r.jour = '$jour_fr';
 " | while read mac debut fin
 do
-    if [[ "$heure" > "$debut" && "$heure" < "$fin" ]]; then
+    if [[ "$heure_actuelle" > "$debut" && "$heure_actuelle" < "$fin" ]]; then
         iptables -C FORWARD -m mac --mac-source "$mac" -j DROP 2>/dev/null
         if [ $? -ne 0 ]; then
             iptables -I FORWARD -m mac --mac-source "$mac" -j DROP
